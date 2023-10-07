5ebeabbcab18f547286dd706776d906e59322ab8
aravinth34 <112575212+aravinth34@users.noreply.github.com>
2023-10-07 14:20:41 +0400

    Update nginx_start_script.sh

--- nginx_start_script.sh
+++ nginx_start_script.sh
@@ -7,7 +7,7 @@ if [ "$getpid" = 0 ]
 then
   echo "Error on Nginx and stoped at" $date >>/home/maravinth/logs_nginx;
   systemctl start nginx
-  sudo fuser -k 443/tcp
+  sudo fuser -k 80/tcp
 
 else
   echo "Nginx is Fine, It is Running at" $date >>/home/maravinth/logs_nginx;
